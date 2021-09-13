7709058142ee23afcf80edbf8b4d4170648337ea
Erik Svensson <erik@familjensvensson.se>
2021-09-13 14:00:00 +0200

    Updates to fix travis testing

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,11 +1,11 @@
-const config = require("./config.json");
+//const config = require("./config.json");
 
-const dsn = process.env.MONGODB_DSN || `mongodb+srv://${config.username}:
-${config.password}@cluster0.jarpc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+//const dsn = process.env.MONGODB_DSN || `mongodb+srv://${config.username}:
+//${config.password}@cluster0.jarpc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const dsn = "mongodb://localhost:27017/test";
 
 if (process.env.NODE_ENV === 'test') {
     // We can even use MongoDB Atlas for testing
-    dsn = "mongodb://localhost:27017/test";
 }
 
 module.exports = {
