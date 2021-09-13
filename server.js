55b633703df54a88b7eb11a3ce02f4cf5e905849
Erik Svensson <erik@familjensvensson.se>
2021-09-13 11:19:46 +0200

    Adds chai and chai-http in preperation for integration tests

--- server.js
+++ server.js
@@ -43,6 +43,8 @@ db.mongoose
 require("./app/routes/textEditor.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+module.exports = server;
