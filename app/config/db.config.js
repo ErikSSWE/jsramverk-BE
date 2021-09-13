38fdf64a98b69e4b049c5993e69458e7e4faea60
Erik Svensson <erik@familjensvensson.se>
2021-09-13 13:37:46 +0200

    Changes for travis testing

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
-//const config = require("./config.json");
+const config = require("./config.json");
 
 const dsn = process.env.MONGODB_DSN || `mongodb+srv://${config.username}:
 ${config.password}@cluster0.jarpc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
